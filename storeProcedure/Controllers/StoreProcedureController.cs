ca2f49599b3d201e57159ba1fe89392a9509c946
Shivam <Shivam123@gmail.com>
2022-03-09 19:31:25 +0530

    Add project files.

---
+++ storeProcedure/Controllers/StoreProcedureController.cs
@@ -0,0 +1,45 @@
+﻿using storeProcedure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace storeProcedure.Controllers
+{
+    public class StoreProcedureController : Controller
+    {
+        // GET: StoreProcedure
+        shivamtestdbEntities1 testdb = new shivamtestdbEntities1();
+        public ActionResult Index()
+        {
+            return View(testdb.sp_select_Branch());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+       [HttpPost]
+        public ActionResult Create(SpData student)
+        {
+            testdb.sp_insert_Branch(student.Id, student.Name, student.Email);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+           var data= testdb.sp_Find_Branch(id).FirstOrDefault();          
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Edit(SpData student)
+        {
+            testdb.sp_update_Branch(student.Id,student.Name,student.Email);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+             testdb.sp_Delete_Branch(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
